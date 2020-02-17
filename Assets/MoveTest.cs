25ca09172c903531610ae28ebc08b1e958b7702d
Andre Cardoso <amc.acardoso@gmail.com>
2020-02-17 11:53:30 +0100

    initial commit

---
+++ Assets/MoveTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTest : MonoBehaviour
+{
+
+    public RaySearch raySearch;
+    public int index;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position,raySearch.meshPoints[index].position,.5f);
+        transform.forward = Vector3.Lerp(transform.forward, raySearch.meshPoints[index].normal,.1f);
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            index++;
+            if (index > raySearch.meshPoints.Count - 1)
+                index = 0;
+        }
+    }
+}
